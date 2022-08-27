604a1431366c7e5757b13d4a5e9f560dee3b60e4
AhilanAshwin <ahilanaashwin@gmail.com>
2022-08-27 12:26:19 +0800

    edit mangum

--- app/main.py
+++ app/main.py
@@ -1,15 +1,9 @@
 from fastapi import FastAPI
 from mangum import Mangum
 
-from fastapi_events.dispatcher import dispatch
-from fastapi_events.middleware import EventHandlerASGIMiddleware
-from fastapi_events.handlers.aws import SQSForwardHandler
-
-app = FastAPI(title="Whats App Blaster API", root_path="/dev/")
-app.add_middleware(EventHandlerASGIMiddleware,
-                   handlers=[SQSForwardHandler(queue_url="awslambda-fastapi-dev-sqs",
-                                               region_name="ap-southeast-1")])
-handler = Mangum(app)
+# app = FastAPI(title="Whats App Blaster API", root_path="/dev/")
+app = FastAPI(title="Whats App Blaster API")
+handler = Mangum(app, api_gateway_base_path="/dev/")
 
 
 @ app.get("/", status_code=200)
@@ -24,4 +18,4 @@ def healthcheck():
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=80, log_level="debug")
+    uvicorn.run(app, host="0.0.0.0", port=8080, log_level="debug")
