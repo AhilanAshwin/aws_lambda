4c9dd81655e3f32505839f862162ceaf6743e0c1
AhilanAshwin <ahilanaashwin@gmail.com>
2022-08-27 13:31:08 +0800

    Fastapi queue

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
+from sys import displayhook
 from fastapi import FastAPI
 from mangum import Mangum
 
+from fastapi_events.dispatcher import dispatch
+from fastapi_events.handlers.aws import SQSForwardHandler
+from fastapi_events.middleware import EventHandlerASGIMiddleware
+
 app = FastAPI(title="Whats App Blaster API", root_path="/dev/")
+app.add_middleware(EventHandlerASGIMiddleware,
+                   handlers=[SQSForwardHandler(queue_url="awslambda-fastapi-dev-sqs",
+                                               region_name="ap-southeast-1")])
 handler = Mangum(app)
 
 
@@ -12,6 +20,7 @@ def get_index():
 
 @ app.get('/ping', status_code=200)
 def healthcheck():
+    dispatch('healthcheck', payload={'message': "Itworks"})
     return {'status': "Success"}
 
 
