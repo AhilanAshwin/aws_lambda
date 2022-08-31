d1aba519a830ff883b92fb1fc68323e71f8bc172
AhilanAshwin <ahilanaashwin@gmail.com>
2022-08-31 18:17:32 +0800

    testing

--- app/main.py
+++ app/main.py
@@ -6,7 +6,9 @@ from app.config import get_settings
 
 settings = get_settings()
 root_path = "/" if settings.STAGE == 'local' else f"/{settings.STAGE}/"
-app = FastAPI(title=f"{settings.PROJECT_NAME} ({settings.STAGE.upper()})", root_path=root_path,
+app = FastAPI(title=f"{settings.PROJECT_NAME} ({settings.STAGE.upper()})",
+              root_path=root_path,
+              root_path_in_servers=False
               description="""An API Service created to blast promotional 
                             messages to prospective clients using Whats App.""")
 handler = Mangum(app)
