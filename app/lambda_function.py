6d6ac85ab2376f609663f917baab0189777c8a16
AhilanAshwin <ahilanaashwin@gmail.com>
2022-09-02 13:35:48 +0800

    Testing network connectivity on lambda

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -6,9 +6,5 @@ def consumer_handler(event, context):
     # NOTE: Do not change the namee of this function
     print("HELLO!")
     print("In lambda")
-    try:
-        r = requests.get("https://google.com")
-        return r.status_code
-    except Exception as e:
-        print(e)
-    return "DONE"
+    r = requests.get("https://google.com")
+    return r.status_code
